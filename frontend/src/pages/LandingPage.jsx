b07052f01b23878f0c5825c86a7059101b6a71f4
xswift-coder770 <155413497+xswift-coder770@users.noreply.github.com>
2025-07-12 17:00:17 +0530

    Update LandingPage.jsx

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -9,7 +9,7 @@ const LandingPage = () => {
     <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center relative">
       
       {/* Splash Cursor */}
-      <SplashCursor />
+      <SplashCursor/>
 
       {/* Login Button Top Left */}
       <button
